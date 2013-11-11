f3e4f89c0c81e364add000547711d0a7769ca021
Nicholas Dipatri <ndipatri@lifeshield.com>
2013-11-11 15:26:25 -0500

    doesn't build.. but starting bluetooth manager!

--- src/main/java/com/ndipatri/activity/RoboLiftActivity.java
+++ src/main/java/com/ndipatri/activity/RoboLiftActivity.java
@@ -1,14 +1,26 @@
 package com.ndipatri.activity;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.ndipatri.R;
 
 public class RoboLiftActivity extends Activity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_robolift);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_robolift);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth not supported on this device!", Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
+
+
+    }
 }
