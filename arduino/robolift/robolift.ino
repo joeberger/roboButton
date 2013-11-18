851a01b8dcab91f0c55f82eea5262482906716f7
ndipatri <ndipatri@lifeshield.com>
2013-11-18 06:50:14 -0500

    Two way communication protocol established! (not tested :-(  )

---
+++ arduino/robolift/robolift.ino
@@ -0,0 +1,73 @@
+/*
+  Example Bluetooth Serial Passthrough Sketch
+ by: Jim Lindblom
+ SparkFun Electronics
+ date: February 26, 2013
+ license: Public domain
+
+ This example sketch converts an RN-42 bluetooth module to
+ communicate at 9600 bps (from 115200), and passes any serial
+ data between Serial Monitor and bluetooth module.
+ */
+#include <SoftwareSerial.h>  
+
+int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
+int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+int localState1 = 0; // default state value
+
+String inputBuffer = "";
+
+void setup()
+{
+  Serial.begin(9600);  // Begin the serial monitor at 9600bps
+
+  bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
+  bluetooth.print("$");  // Print three times individually
+  bluetooth.print("$");
+  bluetooth.print("$");  // Enter command mode
+  delay(100);  // Short delay, wait for the Mate to send back CMD
+  bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
+  // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
+  bluetooth.begin(9600);  // Start bluetooth serial at 9600
+}
+
+void loop()
+{  
+  if(bluetooth.available())  // If the bluetooth sent any characters
+  {
+    char incomingByte = (char)bluetooth.read();
+    inputBuffer += incomingByte;
+     
+    Serial.print(inputBuffer);      
+     
+    if (inputBuffer.startsWith("@@@")) {
+  
+      Serial.print("Received StateReport request!);      
+
+      // we've received a request for local state report!
+      bluetooth.print("@@@" + localState1);
+      inputBuffer = "";
+    }  
+    
+    if (inputBuffer.startsWith("XXX") && inputBuffer.length() == 4) {
+  
+      localState1 = inputBuffer.chatAt(3).toInt();
+      
+      Serial.print("Received StateChange request! Changed to '" + localState1 + "'.");      
+
+      // NJD TODO  - send acknowledgement
+      
+      inputBuffer = "";
+    }    
+}
+  
+  if(Serial.available())  // If stuff was typed in the serial monitor
+  {
+    // Send any characters the Serial monitor prints to the bluetooth
+    bluetooth.print((char)Serial.read());
+  }
+  // and loop forever and ever!
+}
--- src/main/java/com/ndipatri/activity/MainControllerActivity.java
+++ src/main/java/com/ndipatri/activity/MainControllerActivity.java
@@ -16,24 +16,29 @@ import android.widget.ToggleButton;
 import com.ndipatri.R;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 import butterknife.InjectView;
 import butterknife.Views;
+import sun.jvm.hotspot.interpreter.BytecodeGetField;
 
 public class MainControllerActivity extends Activity {
 
     public static final String EXTERNAL_BLUETOOTH_DEVICE_MAC = "macAddress";
 
+    private static final String MY_UUID = "00001101-0000-1000-8000-00805F9B34FB";
+
     private static final String TAG = MainControllerActivity.class.getCanonicalName();
 
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothSocket bluetoothSocket;
+    private BluetoothDevice selectedBluetoothDevice;
 
     protected @InjectView(R.id.textView) TextView textView;
     protected @InjectView(R.id.progressBar) android.widget.ProgressBar progressBar;
     protected @InjectView(R.id.toggleButton) ToggleButton toggleButton;
-    protected @InjectView(R.id.toggleButton2) ToggleButton toggleButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,70 +49,185 @@ public class MainControllerActivity extends Activity {
 
         // NJD TODO - Safe to assume since our prevous Activity hooked us up? We should have BroadcastReceiver monitoring this maybe?
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        selectedBluetoothDevice = getIntent().getParcelableExtra(EXTERNAL_BLUETOOTH_DEVICE_MAC);
 
-        BluetoothDevice selectedBluetoothDevice = getIntent().getParcelableExtra(EXTERNAL_BLUETOOTH_DEVICE_MAC);
         Toast.makeText(this, "Bluetooth Device Selected: '" + selectedBluetoothDevice + "'.", Toast.LENGTH_SHORT).show();
 
         setupViews();
 
-        // Forge socket with bluetooth device..
-        new CreateClientSocketTask().execute(selectedBluetoothDevice);
+        new RetrieveRemoteStateTask().execute();
     }
 
     private void setupViews() {
         toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.d(TAG, "check changed: " + isChecked);
-
-                // NJD TODO - send data on socket!
+                new UdpateRemoteStateTask().execute();
             }
         });
     }
 
-    private class CreateClientSocketTask extends AsyncTask<BluetoothDevice, Void, BluetoothSocket> {
+    private class RetrieveRemoteStateTask extends AsyncTask<Void, String, ByteBuffer> {
 
         @Override
         protected void onPreExecute() {
             progressBar.setVisibility(View.VISIBLE);
-            textView.setText(getString(R.string.opening_bluetooth_socket));
             toggleButton.setVisibility(View.GONE);
-            toggleButton2.setVisibility(View.GONE);
         }
 
         @Override
-        protected BluetoothSocket doInBackground(BluetoothDevice... params) {
-            final BluetoothDevice bluetoothDevice = params[0];
-            BluetoothSocket bluetoothSocket = null;
+        protected ByteBuffer doInBackground(Void... Void) {
+
+            ByteBuffer byteBuffer = null;
 
             try {
-                bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(UUID.nameUUIDFromBytes(new byte[] {(byte)1, (byte)1, (byte)0, (byte)1}));
+                if (bluetoothSocket != null && bluetoothSocket.isConnected()) {
+                    publishProgress(getString(R.string.opening_bluetooth_socket));
+                    bluetoothSocket = createConnectionToBluetoothDevice(bluetoothAdapter, selectedBluetoothDevice);
+                }
 
-                // Cancel discovery because it will slow down the connection
-                bluetoothAdapter.cancelDiscovery();
+                // Tell Arduino to send us StateReport
+                OutputStream outputStream = bluetoothSocket.getOutputStream();
+                outputStream.write(new byte[] {0x40, 0x40, 0x40}); // '@@@'
 
-                // Connect the device through the socket. This will block
-                // until it succeeds or throws an exception
-                bluetoothSocket.connect();
+                // For now, assume one byte state response...
+                final byte[] remoteStateBytes = new byte[1];
+                final int bytesRead = bluetoothSocket.getInputStream().read(remoteStateBytes);
+
+                if (bytesRead != 1) {
+                    publishProgress(getString(R.string.transmission_failure));
+                    return null;
+                }
+
+                publishProgress(getString(R.string.communication_established));
+
+                byteBuffer = ByteBuffer.allocate(1);
+                byteBuffer.put(remoteStateBytes);
+                byteBuffer.flip();  // prepare for reading.
+
             } catch (IOException connectException) {
                 // Unable to connect; close the socket and get out
+                publishProgress(getString(R.string.transmission_failure));
+                Log.d(TAG, "Socket connect exception!", connectException);
                 try { bluetoothSocket.close(); } catch (IOException closeException) {}
-                bluetoothSocket = null;
             }
 
-            return bluetoothSocket;
+            return byteBuffer;
+        }
+
+        protected void onProgressUpdate(String... progress) {
+            textView.setText(progress[0]);
         }
 
         @Override
-        protected void onPostExecute(BluetoothSocket bluetoothSocket) {
-            super.onPostExecute(bluetoothSocket);
+        protected void onPostExecute(ByteBuffer byteBuffer) {
+            super.onPostExecute(byteBuffer);
 
-            MainControllerActivity.this.bluetoothSocket = bluetoothSocket;
-
-            progressBar.setVisibility(View.INVISIBLE);
-            textView.setText(getString(R.string.main_controller));
-            toggleButton.setVisibility(View.VISIBLE);
-            toggleButton2.setVisibility(View.VISIBLE);
+            renderLocalViewsFromRemoteState(byteBuffer);
         }
+    }
+
+    /**
+     * This renders local views from remote state provided.
+     */
+    private void renderLocalViewsFromRemoteState(ByteBuffer byteBuffer) {
+
+        // For now, assume single byte state
+
+        int state = Integer.valueOf(String.valueOf(new char[] {(char)byteBuffer.get()}));
+        if (state > 0) {
+            Log.d(TAG, "State is ON");
+            toggleButton.setChecked(true);
+        } else {
+            Log.d(TAG, "State is OFF");
+            toggleButton.setChecked(false);
+        }
+
+        toggleButton.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+        textView.setText(getString(R.string.main_controller));
+    }
+
+    private class UdpateRemoteStateTask extends AsyncTask<Void, String, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(MainControllerActivity.this, getString(R.string.transmitting_data), Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... Void) {
+
+            try {
+                if (bluetoothSocket != null && bluetoothSocket.isConnected()) {
+                    publishProgress(getString(R.string.opening_bluetooth_socket));
+                    bluetoothSocket = createConnectionToBluetoothDevice(bluetoothAdapter, selectedBluetoothDevice);
+                }
+
+                ByteBuffer desiredState = renderRemoteStateFromLocalViews();
+
+                OutputStream outputStream = bluetoothSocket.getOutputStream();
+                outputStream.write(new byte[] {0x58, 0x58, 0x58}); // 'XXX'
+                outputStream.write(desiredState.array());
+
+                publishProgress(getString(R.string.transmission_success));
+
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                publishProgress(getString(R.string.transmission_failure));
+                Log.d(TAG, "Socket connect exception!", connectException);
+                try { bluetoothSocket.close(); } catch (IOException closeException) {}
+            }
+
+            return null;
+        }
+
+        protected void onProgressUpdate(String... progress) {
+            Toast.makeText(MainControllerActivity.this, progress[0], Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private ByteBuffer renderRemoteStateFromLocalViews() {
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1);
+
+        if (toggleButton.isChecked()) {
+            byteBuffer.put((byte)'1'); // a char is the ascii representation of '1'
+        } else {
+            byteBuffer.put((byte)'0');
+        }
+
+        // prepare for reading
+        byteBuffer.flip();
+
+        return byteBuffer;
+    }
+
+    // Should not be run in UI thread.
+    private static BluetoothSocket createConnectionToBluetoothDevice(BluetoothAdapter bluetoothAdapter, BluetoothDevice bluetoothDevice) {
+
+        BluetoothSocket bluetoothSocket = null;
+
+        try {
+            Log.d(TAG, "Creating Bluetooth Socket ...");
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(UUID.fromString(MY_UUID));
+
+            // Cancel discovery because it will slow down the connection
+            bluetoothAdapter.cancelDiscovery();
+
+            // Connect the device through the socket. This will block
+            // until it succeeds or throws an exception
+            bluetoothSocket.connect();
+
+            Log.d(TAG, "Success!");
+
+        } catch (IOException connectException) {
+
+            Log.e(TAG, "Failed with Exception!", connectException);
+            try { bluetoothSocket.close(); } catch (IOException closeException) {}
+            bluetoothSocket = null;
+        }
+
+        return bluetoothSocket;
     }
 }
