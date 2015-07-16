0c47b8fb8e067033f406b4d529ccda545ca7211d
Nicholas DiPatri <ndipatri@gmail.com>
2015-07-16 09:52:37 -0400

    Only detect a specific Estimate beacon for the demo!

--- app/src/main/java/com/ndipatri/roboButton/utils/RegionUtils.java
+++ app/src/main/java/com/ndipatri/roboButton/utils/RegionUtils.java
@@ -5,7 +5,7 @@ import com.ndipatri.roboButton.models.Region;
 public class RegionUtils {
     public static final String GELO_UUID = "11e44f094ec4407e9203cf57a50fbce0";
     public static final String LIGHTBLUE_UUID = "a495ff10c5b14b44b5121370f02d74de";
-    public static final String ESTIMOTE_UUID = "b9407f30f5f8466eaff925556b57fe6d";
+    public static final String ESTIMOTE_UUID = "b9407f30f5f8466e211225556b57fe6d";
 
     // TODO - For now, we will assume homogenous regions: same beacon types in any paritcular region.
 
