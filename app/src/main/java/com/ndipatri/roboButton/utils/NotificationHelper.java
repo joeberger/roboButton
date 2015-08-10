83e9447ce4743fa199bfc8bf489aedd10583836b
Nicholas DiPatri <ndipatri@gmail.com>
2015-08-10 09:07:53 -0400

    Notifications now support multiple simultaneous buttons.

--- app/src/main/java/com/ndipatri/roboButton/utils/NotificationHelper.java
+++ app/src/main/java/com/ndipatri/roboButton/utils/NotificationHelper.java
@@ -48,21 +48,14 @@ public class NotificationHelper {
         // construct the Notification object.
         Notification.Builder builder = new Notification.Builder(context);
         builder.setWhen(System.currentTimeMillis());
-        //builder.setContentText(tickerText);
         builder.setSmallIcon(buttonState.smallDrawableResourceId); // this is what shows up in notification bar before you pull it down
-        builder.setNumber(1234);
         builder.setAutoCancel(true);
         builder.setOnlyAlertOnce(true);
-        ///builder.setContentIntent(pendingIntent);
         builder.setVibrate(new long[]{0,     // start immediately
                 200,   // on
                 1000,  // off
                 200,   // on
                 -1});  // no repeat
-
-        // This provides sub-information in the notification. not using right now.
-        //String notifyTitle = "Test app for ORMLite";
-        //builder.addAction(R.drawable.green_button_small, notifyTitle, pendingIntent);
 
         Notification notification = builder.build();
 
@@ -74,10 +67,10 @@ public class NotificationHelper {
         Intent serviceIntent = new Intent(context, MonitoringService.class);
         serviceIntent.putExtra(MonitoringService.SHOULD_TOGGLE_FLAG, true);
         serviceIntent.putExtra(MonitoringService.BUTTON_ID, buttonId);
-        PendingIntent togglePendingIntent = PendingIntent.getService(context, 0, serviceIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent togglePendingIntent = PendingIntent.getService(context, notifId, serviceIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         Intent activityIntent = new Intent(context, MainActivity.class);
-        PendingIntent launchPendingIntent = PendingIntent.getActivity(context, 0, activityIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent launchPendingIntent = PendingIntent.getActivity(context, notifId, activityIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         contentView.setOnClickPendingIntent(R.id.detailTextView, togglePendingIntent);
         contentView.setOnClickPendingIntent(R.id.buttonImageView, launchPendingIntent);
